41e089e4adc65a38599e69bf5e1dc5056f0929f8
FRANCISCO GASPERINI RIBAS <francisco_ribas@estudante.sesisenai.org.br>
2024-09-20 09:52:03 -0300

    100%funcional

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 import 'dotenv/config';
 import postgres from 'postgres';
 
-let { PGHOST, PGDATABASE, PGAPIECO, PGPASSWORD } = process.env;
+let { PGHOST, PGDATABASE, PGPARQUEFELIZ, PGPASSWORD } = process.env;
 
 const sql = postgres({
   host: PGHOST,
   database: PGDATABASE,
-  username: PGAPIECO,
+  username: PGPARQUEFELIZ,
   password: PGPASSWORD,
   port: 5432,
   ssl: false,
